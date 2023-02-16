5bf1b40a575061d19e4a7fdbeb06a1b994ad36c7
Anton Prokhorov <varpmen@gmail.com>
2023-02-17 02:53:03 +0300

    fix/исправить белые поля страницы

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,7 +12,7 @@ function Layout() {
   return (
     <div className="layout">
       <Header />
-      <main>
+      <main className="main">
         <Outlet />
       </main>
       {isAccessFooterRoute && <Footer />}
